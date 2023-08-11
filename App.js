e9ae8a5ad6d518ca02057de5c28bea05ec391341
PauloAngeloHS <134169822+PauloAngeloHS@users.noreply.github.com>
2023-08-11 11:02:33 +0000

    saved

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [originCurrency, setOriginCurrency] = useState("BRL");
+  const [bananaCurrency, setBananaCurrency] = useState("USD");
+  const [value, setValue] = useState("33.33");
+  const [result, setResult] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Conversor de Moedas</Text>
+      <View>
+        <Text style={styles.label}>Moeda 1</Text>
+        <Picker selectedValue={originCurrency} onValueChange={(itemValue, itemIndex) => setOriginCurrency(itemValue)}>
+          <Picker.Item label="Brazilian Real" value="BRL" />
+          <Picker.Item label="American Dolar" value="USD" />
+        </Picker>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+  },
+  picker: {
+    color: "#fff",
+  },
+  label: {
+    color: "#fff",
+  },
+  input: {
+    color: "#fff",
   },
 });
